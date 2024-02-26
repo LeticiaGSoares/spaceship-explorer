0b46f29809d61f7ef86a8bd9fcea9b5ea363bab7
Letícia G. Soares <leticiag.soares.dev@gmail.com>
2024-02-26 10:57:03 -0300

    solar-system js update

--- assets/js/solar-system.js
+++ assets/js/solar-system.js
@@ -1,20 +1,51 @@
 import { get } from "./api.js";
 
 const planetsQueue = [
-    'sun', 
-    'mercury', 
-    'venus', 
-    'earth', 
-    'mars', 
-    'jupiter', 
-    'saturn', 
-    'uranus', 
-    'neptune'
+    'Mercury', 
+    'Venus', 
+    'Earth', 
+    'Mars', 
+    'Jupiter', 
+    'Saturn', 
+    'Uranus', 
+    'Neptune'
 ]
+
 const arrowLeft = document.querySelector('#arrow-left')
 const arrowRight = document.querySelector('#arrow-right')
-const planetOn = document.querySelector('')
+const planetName = document.querySelector('#planet-name')
+const bodyType = document.querySelector('#body-type')
+const planetImg = document.querySelector('#planet-img')
+const avgTemp = document.querySelector('#avg-temp')
+const gravity = document.querySelector('#gravity')
+const mass = document.querySelector('#mass')
+const density = document.querySelector('#density')
+const moonCount = document.querySelector('#moon-count')
+const discoveryDate = document.querySelector('#discovery-date')
 
 document.addEventListener('DOMContentLoaded', () =>{
-    const loadPlanet = get(`solar-system`, `${planetsQueue[3]}`)
+    const loadPlanet = get(`solar-system`, `${planetsQueue[2]}`)
+    
+    loadPlanet.then((data) =>{
+        console.log(data)
+        planetName.textContent = planetsQueue[2]
+        bodyType.textContent = data.bodyType
+        avgTemp.textContent = `${data.avgTemp} K`
+        gravity.textContent = data.gravity.toFixed(1)
+        mass.textContent = `${data.mass.massValue.toFixed(1)}*10^${data.mass.massExponent}`
+        density.textContent = data.density.toFixed(1)
+
+        if(data.moons === null){
+            moonCount.textContent = '0'
+        }else{
+            moonCount.textContent = data.moons.length
+        }
+
+        if(data.discoveryDate === ""){
+            discoveryDate.textContent = "Since ever"
+        }else{
+            discoveryDate.textContent = data.discoveryDate
+        }
+        
+    })
 })
